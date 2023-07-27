9fa7c591a6c29297cdc5fbd1d687d86d07e2e519
Ibrahim Ismailji <57590567+ibrashub@users.noreply.github.com>
2023-07-27 04:35:17 +0200

    tried to implement camera function

--- app/src/main/java/infinuma/android/shows/ui/shows/ShowsFragment.kt
+++ app/src/main/java/infinuma/android/shows/ui/shows/ShowsFragment.kt
@@ -19,14 +19,12 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.widget.ImageView
+import android.util.Log
 import androidx.core.content.FileProvider
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.google.android.material.button.MaterialButton
 import infinuma.android.shows.FileUtil
 import infinuma.android.shows.R
 import infinuma.android.shows.data.model.ShowsViewModel
@@ -35,7 +33,6 @@ import infinuma.android.shows.databinding.FragmentShowsBinding
 import infinuma.android.shows.ui.login.REMEMBER_ME
 import infinuma.android.shows.ui.login.USER_EMAIL
 import java.io.File
-import kotlinx.coroutines.launch
 
 class ShowsFragment : Fragment(R.layout.fragment_shows) {
 
@@ -64,15 +61,11 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
 
         checkIfPermissionNeeded()
 
-//        val sharedPreferences by lazy {
-//            requireContext().getSharedPreferences("my_preferences", Context.MODE_PRIVATE)
-//        }
         val imageFilePath = sharedPreferences.getString("image_path", null)
         if (imageFilePath != null) {
             val imageUri = Uri.fromFile(File(imageFilePath))
             binding.showsProfilePhoto.setImageURI(imageUri)
         }
-
 
         viewModel.showsLiveData.observe(viewLifecycleOwner) { showsList ->
             adapter = ShowsAdapter(showsList) { show ->
@@ -112,7 +105,6 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
         val imageUriString = sharedPreferences.getString("image_path", null)
         val userEmail =
             sharedPreferences.getString(USER_EMAIL, R.string.default_email.toString()) ?: USER_EMAIL
-        //val userProfilePhoto = R.drawable.ic_profile_placeholder
 
         if (imageUriString != null) {
             val imageUri = Uri.parse(imageUriString)
@@ -120,7 +112,6 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
         }
 
         binding.emailTextProfile.text = userEmail
-        //
 
         binding.changePhotoButton.setOnClickListener {
             val builder = AlertDialog.Builder(context)
@@ -128,10 +119,6 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
                 .setTitle("Choose")
                 .setMessage("Take a new photo")
                 .setPositiveButton("Camera") { dialog, _ ->
-                    //                    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    //                    startActivity(cameraIntent)
-                    ActivityResultContracts.TakePicture()
-
                     takeImage()
                     dialog.dismiss()
                 }
@@ -166,11 +153,8 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
                 .setNegativeButton(R.string.no) { dialog, _ ->
                     dialog.dismiss()
                 }
-
             builder.show()
-
         }
-
         dialog.show()
     }
 
@@ -188,21 +172,14 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         val grantedPermissions = permissions.filterValues { it }.keys
         if (grantedPermissions.contains(Manifest.permission.READ_MEDIA_IMAGES) &&
-            grantedPermissions.contains(Manifest.permission.READ_MEDIA_VIDEO) &&
-            grantedPermissions.contains(Manifest.permission.READ_MEDIA_AUDIO)) {
-            // All necessary permissions have been granted. You can now access media files.
-            // For example, you can call the selectImageFromGalleryResult.launch() method here.
-        } else {
-            // Some or all permissions were denied. Handle the situation accordingly.
-            // You can show a message to the user explaining why the permissions are needed.
-        }
+            grantedPermissions.contains(Manifest.permission.READ_MEDIA_VIDEO)) {
+        } else {}
     }
 
     private fun requestCameraPermission() {
         val permissions = arrayOf(
             Manifest.permission.READ_MEDIA_IMAGES,
-            Manifest.permission.READ_MEDIA_VIDEO,
-            Manifest.permission.READ_MEDIA_AUDIO
+            Manifest.permission.READ_MEDIA_VIDEO
         )
         requestPermissionLauncher.launch(permissions)
     }
@@ -220,36 +197,63 @@ class ShowsFragment : Fragment(R.layout.fragment_shows) {
         }
     }
 
-
-
     private var latestTmpUri: Uri? = null
 
     private fun takeImage() {
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                getTmpFileUri().let { uri ->
-                    latestTmpUri = uri
-                    selectImageFromGalleryResult.launch("image/*")
+        val tmpFileUri = getTmpFileUri()
+        latestTmpUri = tmpFileUri // Set the latestTmpUri to the current tmpFileUri
 
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, tmpFileUri)
+        takePictureResult.launch(takePictureIntent)
+
+        Log.d("ShowsFragment", "LatestTmpUri: $latestTmpUri") // Add this line for debugging
+    }
+
+    private val takePictureResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val imageUri = latestTmpUri ?: run {
+                Log.e("ShowsFragment", "LatestTmpUri is null.")
+                return@registerForActivityResult
+            }
+            try {
+                val imageFile = File(imageUri.path)
+                if (imageFile.exists()) {
+                    // Save the image file to a permanent location
+                    val imageFileName = "profile_photo.jpg"
+                    val destFile = File(requireContext().filesDir, imageFileName)
+                    imageFile.copyTo(destFile, overwrite = true)
+
+                    // Save the image file path in SharedPreferences
                     val sharedPreferences = requireContext().getSharedPreferences(USER_EMAIL, Context.MODE_PRIVATE)
                     sharedPreferences.edit {
-                        putString("image_uri", uri.toString())
+                        putString("image_path", destFile.absolutePath)
                     }
+
+                    // Update the ImageView with the new profile photo
+                    val bitmap = BitmapFactory.decodeFile(destFile.absolutePath)
+                    binding.showsProfilePhoto.setImageBitmap(bitmap)
+                } else {
+                    Log.e("ShowsFragment", "Image file does not exist at path: ${imageUri.path}")
                 }
+            } catch (e: Exception) {
+                Log.e("ShowsFragment", "Error while copying image: ${e.message}")
             }
+
         }
     }
 
     private fun selectImageFromGallery() = selectImageFromGalleryResult.launch("image/*")
 
     private fun getTmpFileUri(): Uri {
-        val tmpFile = File.createTempFile("tmp_image_file", ".png", requireContext().cacheDir).apply {
+        val tmpFileName = "tmp_image_file.png"
+        val tmpFile = File(requireContext().cacheDir, tmpFileName).apply {
             createNewFile()
             deleteOnExit()
         }
-
         return FileProvider.getUriForFile(
-            requireContext().applicationContext, "${requireContext().packageName}.provider",
+            requireContext().applicationContext,
+            "${requireContext().packageName}.provider",
             tmpFile
         )
     }
